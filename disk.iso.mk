44e5c67be6ea9d881fe849d0b48a532f6cd06481
Zach Deibert <zachdeibert@gmail.com>
2017-04-16 14:42:00 -0500

    Made /tmp bind mount

--- disk.iso.mk
+++ disk.iso.mk
@@ -1,17 +1,20 @@
-all: data/com.github.centaurios.application-control/bin/appctl dev proc tmp sys
+all: data/com.github.centaurios.application-control/bin/appctl dev proc tmp sys data/com.github.centaurios.application-control/tmp
 
 dev:
-	mkdir -p dev
+	mkdir -p $@
 
 proc:
-	mkdir -p proc
+	mkdir -p $@
 
 tmp:
-	mkdir -p tmp
+	mkdir -p $@
 
 sys:
-	mkdir -p sys
+	mkdir -p $@
 
 data/com.github.centaurios.application-control/bin/appctl: ../appctl/appctl
 	mkdir -p data/com.github.centaurios.application-control/bin
 	cp -f $< $@
+
+data/com.github.centaurios.application-control/tmp:
+	mkdir -p $@
